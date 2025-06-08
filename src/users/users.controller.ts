1a0d5a2854672f8c8e71d6b19666c3ee86a51f3b
Amandeep Singh <programmeramandeep@gmail.com>
2025-06-08 17:27:19 +0530

    feat: Params, Query and Body

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/:id{/:optional}') // For adding optional path parameters, then use curly braces for example: @Get('{/:id}')
+  public getUsers(@Param() params: { id: string }, @Query() query: any) {
+    console.log('Params:', params);
+    console.log('Query:', query);
+
+    return 'List of users';
+  }
+
+  @Post()
+  public createUser(@Body() body: any) {
+    console.log('Request Body:', body);
+    return 'User created';
+  }
+}
